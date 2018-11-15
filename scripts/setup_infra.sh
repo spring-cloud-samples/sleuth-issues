eb34eed8952c89f9b3aa56a1327755509925709b
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2018-11-15 17:42:25 +0100

    Better docker killing

--- scripts/setup_infra.sh
+++ scripts/setup_infra.sh
@@ -2,10 +2,12 @@
 
 export IP="$( ./scripts/whats_my_ip.sh )"
 
+./scripts/stop_infra.sh
+
 echo "Building docker"
-pushd docker
-docker-compose build
-docker-compose up -d
-popd
-echo "Waiting 5 seconds for kafka to start"
+  pushd docker
+  docker-compose build
+  docker-compose up -d
+  popd
+  echo "Waiting 5 seconds for kafka to start"
 sleep 5
--- scripts/stop_infra.sh
+++ scripts/stop_infra.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 pushd docker
-docker-compose kill || echo "Failed to kill docker"
-yes | docker-compose rm -v || echo "Failed to remove volumes"
+  docker-compose kill || echo "Failed to kill docker"
+  yes | docker-compose rm -v || echo "Failed to remove volumes"
+  docker stop $(docker ps | grep zookeeper| awk -F' ' '{print $1}') || echo "Failed to stop zookeeper"
+  docker stop $(docker ps | grep kafka | awk -F' ' '{print $1}') || echo "Failed to stop kafka"
 popd
