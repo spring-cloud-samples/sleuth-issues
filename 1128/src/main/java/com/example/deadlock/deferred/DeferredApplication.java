9cd839b04590260580d98208857a4fdb8a4da0cf
Marcin Grzejszczak <marcin@grzejszczak.pl>
2018-12-19 18:22:15 +0100

    Added 1128

---
+++ 1128/src/main/java/com/example/deadlock/deferred/DeferredApplication.java
@@ -0,0 +1,12 @@
+package com.example.deadlock.deferred;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeferredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeferredApplication.class, args);
+	}
+}
